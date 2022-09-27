c2bcc41d06416714835395928effca3f89795735
RubinoD1 <Rubinod1@gmail.com>
2022-09-27 11:42:24 -0400

    added login/add user and save/remove book muttations to mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,2 +1,65 @@
 import { gql } from '@apollo/client';
 
+// Executes the loginUser mutation
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// Executes the addUser mutation
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// Executes the saveBook mutation 
+export const SAVE_BOOK = gql`
+  mutation saveBook($book: SavedBookInput!) {
+    saveBook(book: $book) {
+      username
+      email
+      bookCount
+      savedBooks {
+        authors
+        description
+        bookId
+        image
+        link
+        title
+      }
+    }
+  }
+`;
+
+// Executes the removeBook mutation
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      username
+      email
+      bookCount
+      savedBooks {
+        authors
+        description
+        bookId
+        image
+        link
+        title
+      }
+    }
+  }
+`;
